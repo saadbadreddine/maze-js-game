0fd77f2a71dcdd890751b4fdaa0ce24a539b789e
Saad <badreddinesaad91@gmail.com>
2022-01-23 03:16:27 +0200

    almost done; must clean; and remove won/lost message on reset or start over

--- game.js
+++ game.js
@@ -4,6 +4,8 @@
 	var boundaries = document.getElementsByClassName('boundary');
 	var start = document.getElementById('start');
 	var end = document.getElementById('end');
+	var status = document.getElementById('status');
+	boundaries[5].style.textAlign = 'center';
 
 	function startGame() {
 		if (game_state == 0) {
@@ -17,7 +19,7 @@
 			for (var i = 0; i < boundaries.length - 1; i++) {
 				boundaries[i].addEventListener('mouseover', lostGame);
 			}
-		} else {
+		} else if (game_state == 1 || game_state == 2) {
 			game_state = 1;
 			console.log('Game Started');
 			console.log(game_state);
@@ -29,11 +31,19 @@
 		}
 	}
 
+	function endGame() {
+		score = 0;
+		boundaries[5].innerText = score;
+		game_state = 2;
+	}
+
 	function wonGame() {
 		console.log('You Won!!');
-		game_state = 0;
+		game_state = 1;
 		score += 5;
+		boundaries[5].innerText = score;
 		console.log(game_state);
+		status.innerText = 'You won';
 		end.removeEventListener('mouseover', wonGame);
 		for (var i = 0; i < boundaries.length - 1; i++) {
 			boundaries[i].removeEventListener('mouseover', lostGame);
@@ -44,10 +54,12 @@
 		for (var i = 0; i < boundaries.length - 1; i++) {
 			boundaries[i].classList.add('youlose');
 		}
-		console.log('You Lose!!');
+		console.log('You Lost!!');
 		game_state = 0;
 		score -= 10;
+		boundaries[5].innerText = score;
 		console.log(game_state);
+		status.innerText = 'You lost';
 		for (i = 0; i < boundaries.length - 1; i++) {
 			boundaries[i].removeEventListener('mouseover', lostGame);
 		}
@@ -55,5 +67,6 @@
 	}
 
 	start.addEventListener('click', startGame);
+	end.addEventListener('click', endGame);
 }),
 	true;
