9cacb8935bf631bf1bd41b4e0a2eee2f9bb091af
Saad <badreddinesaad91@gmail.com>
2022-01-23 02:15:37 +0200

    added removeEventListener to stop the game when won or lost

--- game.js
+++ game.js
@@ -1,52 +1,51 @@
-mouse_clicked = false;
-var mouse_position;
-
-window.onload = function() {
-	var div_boundaries = document.getElementsByClassName('boundary');
+(window.onload = function() {
+	game_state = 2;
+	var boundaries = document.getElementsByClassName('boundary');
 	var start = document.getElementById('start');
 	var end = document.getElementById('end');
 
-	start.addEventListener(
-		'click',
-		function() {
-			mouse_clicked = true;
-			for (var i = 0; i < div_boundaries.length - 1; i++) {
-				div_boundaries[i].classList.remove('youlose');
+	function startGame() {
+		if (game_state == 0) {
+			for (var i = 0; i < boundaries.length - 1; i++) {
+				boundaries[i].classList.remove('youlose');
 			}
-		},
-		true
-	);
+			game_state = 1;
+		} else {
+			game_state = 1;
+		}
+	}
 
-	document.addEventListener(
-		'mousemove',
-		function(event) {
-			event.preventDefault();
-			if (mouse_clicked) {
-				mouse_position = {
-					x: event.clientX,
-					y: event.clientY
-				};
+	function lostGame() {
+		for (i = 0; i < boundaries.length - 1; i++) {
+			boundaries[i].classList.add('youlose');
+		}
+		console.log('You Lose!!');
+		game_state = 0;
+		console.log(game_state);
+		for (i = 0; i < boundaries.length - 1; i++) {
+			boundaries[i].removeEventListener('mouseover', lostGame);
+		}
+		end.removeEventListener('mouseover', wonGame);
+	}
 
-				for (var i = 0; i < div_boundaries.length - 1; i++) {
-					end.addEventListener('mouseover', function() {
-						console.log('You Won!!');
-					});
-				}
+	function wonGame() {
+		console.log('You Won!!');
+		game_state = 0;
+		console.log(game_state);
+		end.removeEventListener('mouseover', wonGame);
+		for (i = 0; i < boundaries.length - 1; i++) {
+			boundaries[i].removeEventListener('mouseover', lostGame);
+		}
+	}
 
-				for (var i = 0; i < div_boundaries.length - 1; i++) {
-					div_boundaries[i].addEventListener(
-						'mouseover',
-						function() {
-							for (var i = 0; i < div_boundaries.length - 1; i++) {
-								div_boundaries[i].classList.add('youlose');
-							}
-							console.log('You Lose!!');
-						},
-						true
-					);
-				}
-			}
-		},
-		true
-	);
-};
+	start.addEventListener('click', startGame);
+
+	if (game_state == 1) {
+		end.addEventListener('mouseover', wonGame);
+
+		for (i = 0; i < boundaries.length - 1; i++) {
+			boundaries[i].addEventListener('mouseover', lostGame);
+		}
+	}
+}),
+	true;
