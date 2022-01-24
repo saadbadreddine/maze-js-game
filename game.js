e761fe946169de8b6e98b4fc91fab7bf2951e88a
Saad <badreddinesaad91@gmail.com>
2022-01-24 13:00:09 +0200

    adding comments

--- game.js
+++ game.js
@@ -15,14 +15,15 @@
 					 state 0 = game lost
 					 state 1 = game won
 	when the game first initiates (state 2) the user has to move his mouse over the "S" to start the game as instructed
-	When the game starts event listeners, and the event that started the game on mouseover is stopped
+	When the game starts event listeners are created, and the event that started the game on mouseover is stopped
 	those events are: if the mouse moves over the boundaries the users loses
 				  if the mouse moves outside of game block the user is warned and game is stopped
 				  if the mouse reaches the E the user wins only if he doesn't touch the boundaries or gets out of bound. 
 				  if the user wins or loses the game stops 
-				  when the game stops the user has to click on S to start over or can click on E to end game and reset the score (state 2)
+				  when the game stops the user has to click on S to start over or can click on E to end the game and reset the score (state 2)
 				  S and E can't be spammed and are only clickable once, and they're only clickable if the user has won or lost
 */
+
 	function startGame() {
 		if (game_state == 0) {
 			for (var i = 0; i < boundaries.length; i++) {
